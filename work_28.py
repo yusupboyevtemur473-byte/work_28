3b95d85605fbeb34fb3ac9dcee1326b65c777706
TEMURBEK <yusupboyevtemur473@gmail.com>
2026-03-05 14:49:06 +0500

    Add files via upload

---
+++ work_28.py
@@ -0,0 +1,86 @@
+#1-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#while a>b:    
+#    a=a-b
+#print("Bo`sh qismi", a)
+
+#2-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#i=0
+#while a>=b:    
+#   a-=b
+#   i+=1
+#print(i)  
+
+#3-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#i=0
+#while a>=b:    
+#   a-=b
+#   i+=1  
+#print("Bo`sh qismi", i)
+#print("Qoldiq", a)
+
+#4-misol
+#n = int(input("n = "))
+#while n % 3 == 0:
+#    n = n // 3
+#if n == 1:
+#    print("3 ning darajasi")
+#else:
+#    print("3 ning darajasi emas")
+
+
+#5-misol
+#n = int(input("n = "))
+#k = 0
+#while n > 1:
+#    n = n // 2
+#    k += 1
+#print("k =", k)
+
+
+#6-misol
+#n = int(input("n = "))
+#result = 1
+#while n > 0:
+#    result *= n
+#    n -= 2
+#print("Natija =", result)
+
+#7-misol
+#n = int(input("n = "))
+#k = 1
+#while k * k <= n:
+#    k += 1
+#print("k =", k)
+
+#8-misol
+
+#n = int(input("n = "))
+#k = 1
+#while (k + 1) * (k + 1) <= n:
+#    k += 1
+#print("k =", k)
+
+#9-misol
+#n = int(input("n = "))
+#k = 0
+#power = 1
+
+#while power <= n:
+#    power *= 3
+#    k += 1
+#print("k =", k)
+
+#10-misol
+#n = int(input("n = "))
+#k = 0
+#power = 1
+#while power * 3 <= n:
+#    power *= 3
+#    k += 1
+#print("k =", k)
